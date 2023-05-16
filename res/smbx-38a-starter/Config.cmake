1d29f6ff367e35866e3c0a538d1fa7b877b46386
xiaodouxd <lianghengming@live.cn>
2023-05-16 12:02:57 +0800

    敲注释

--- res/smbx-38a-starter/Config.cmake
+++ res/smbx-38a-starter/Config.cmake
@@ -1,2 +1,7 @@
+# vcpkg 根目录
+# vcpkg root path
 set(VCPKG_ROOT D:/ProgramData/vcpkg)
+
+# 是否重复生成资源代码
+# is regenerating resource code while build cmake project
 set(XD_RC_REGEN OFF)
--- res/smbx-38a-starter/src/main.cpp
+++ res/smbx-38a-starter/src/main.cpp
@@ -1,6 +1,11 @@
 #include <string>
 
+// 游戏名称, 决定了数据文件夹名
+// game name, which determines the name of data folder generated
 const std::string gameName = "shadow2d";
+
+// 游戏文件路径, 从 ./$数据文件夹名$/worlds 开始
+// path to game file (start from ./!$gameName$_data/worlds)
 const std::string pathToMain = "main.elvl";
 
 #include "private/main-content.h"
